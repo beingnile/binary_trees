3ed5c2072f9ea1afcf98395b7bc9677420632ae9
Nile Okomo <okomonile3@gmail.com>
2022-02-25 18:34:36 +0300

    Free parent node if NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	if (parent == NULL)
+	{
+		free(parent);
 		return (NULL);
+	}
 
 	left_node->parent = parent;
 	left_node->n = value;
