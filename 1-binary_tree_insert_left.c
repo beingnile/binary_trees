fc6e081e0dad6fed0ce2e4955c4a8372f5da212e
Nile Okomo <okomonile3@gmail.com>
2022-02-25 19:00:12 +0300

    Refactor code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,10 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	left_node = malloc(sizeof(binary_tree_t));
 	if (left_node == NULL)
-	{
-		free(left_node);
 		return (NULL);
-	}
 
 	if (parent == NULL)
 	{
